d3042ef4af7731052a680459f8fa4c2ff78fde7e
johyunjoo99 <chj17200@naver.com>
2022-08-10 18:04:45 +0900

    PORTFOLIO_2022.08.10
    mousemove effect 구현

--- script/index.js
+++ script/index.js
@@ -1,5 +1,71 @@
 $(function(){
 
+    const circle = document.querySelector('.circle')
+
+    let mouseX = 0
+    let mouseY = 0
+
+    let circleX = 0
+    let circleY = 0
+
+    let speed = 0.06
+
+    const animate = () => {
+        let distX = mouseX - circleX
+        let distY = mouseY - circleY
+        let translatedValue = `translate3d(${circleX}px, ${circleY}px, 0 )`;
+
+        circleX = circleX + (distX * speed)
+        circleY = circleY + (distY * speed)
+
+        circle.style.transform = translatedValue;
+
+        requestAnimationFrame(animate)
+    }
+
+    animate()
+
+    document.addEventListener('mousemove', (e) => {
+        mouseX = e.clientX - circle.offsetWidth / 4;
+        mouseY = e.clientY - circle.offsetHeight / 4;
+    })
+
+    // const circle = document.querySelector(".circle")
+
+    // let mouseX = 0
+    // let mouseY = 0
+    // let circleX = 0
+    // let circleY = 0
+    
+    // let speed = 0.06
+    
+    // const animate = () => {
+    //     let distX = mouseX = circleX
+    //     let distY = mouseY = circleY
+    //     let translatedValue = `translate3d(${circleX}px, ${circleY}px, 0 )`;
+    
+    //     circleX = circleX + (distX * speed)
+    //     circleY = circleY + (distY * speed)
+    
+    //     circle.style.transform = translatedValue;
+    
+    //     requestAnimationFrame(animate)
+    
+    // }
+    
+    // animate()
+    
+    // document.addEventListener('mousemove', (e) => {
+    //     mouseX = e.clientX - circle.offsetWidth / 4;
+    //     mouseY = e.clientY - circle.offsetHeight / 4;
+    // })
+
+
+
+
+
+
+    //slider animation
     $(".effect").each(function(){
         $(this).addClass("active");
     })
@@ -52,4 +118,3 @@ $(function(){
     });
 
 })
-    
